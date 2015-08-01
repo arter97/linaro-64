c7727b61f359ec5ddb193943c8bf0eb306053254
Park Ju Hyung <qkrwngud825@gmail.com>
2015-08-01 16:55:45 +0900

    Import linaro-snapshot-6.2-2016.09
    export CFLAGS='-mtune=native -pipe'
    export CXXFLAGS='-mtune=native -pipe'
    ../gcc/configure --prefix=/home/arter97/linaro-64 --prefix=/home/arter97/linaro-64 --host=x86_64-linux-gnu --build=x86_64-linux-gnu --with-gnu-as --with-gnu-ld --enable-languages=c,c++ --disable-ppl-version-check --disable-cloog-version-check --disable-isl-version-check --enable-cloog-backend=isl --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --disable-libssp --disable-nls --disable-libmudflap --disable-shared --disable-sjlj-exceptions --disable-libquadmath --disable-libitm --disable-libsanitizer --disable-docs --target=aarch64-none-elf --enable-threads --enable-tls --disable-libgomp --disable-libstdc__-v3 --disable-libitm --disable-libsanitizer --disable-libatomic --enable-plugins
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

---
+++ lib/gcc/aarch64-none-elf/6.2.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --prefix=/home/arter97/linaro-64 --prefix=/home/arter97/linaro-64 --host=x86_64-linux-gnu --build=x86_64-linux-gnu --with-gnu-as --with-gnu-ld --enable-languages=c,c++ --disable-ppl-version-check --disable-cloog-version-check --disable-isl-version-check --enable-cloog-backend=isl --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --disable-libssp --disable-nls --disable-libmudflap --disable-shared --disable-sjlj-exceptions --disable-libquadmath --disable-libitm --disable-libsanitizer --disable-docs --target=aarch64-none-elf --enable-threads --enable-tls --disable-libgomp --disable-libstdc__-v3 --disable-libitm --disable-libsanitizer --disable-libatomic --enable-plugins";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
