969bdf101cdc746a76aa0b62dda90437f2c1abb6
Park Ju Hyung <qkrwngud825@gmail.com>
2018-05-03 03:08:36 +0900

    Import linaro-snapshot-7.3-2018.04
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- lib/gcc/aarch64-linux-gnu/7.3.1/plugin/include/plugin-version.h
+++ lib/gcc/aarch64-linux-gnu/7.3.1/plugin/include/plugin-version.h
@@ -6,7 +6,7 @@
 #define GCCPLUGIN_VERSION  (GCCPLUGIN_VERSION_MAJOR*1000 + GCCPLUGIN_VERSION_MINOR)
 
 static char basever[] = "7.3.1";
-static char datestamp[] = "20180314";
+static char datestamp[] = "20180425";
 static char devphase[] = "";
 static char revision[] = "";
 
